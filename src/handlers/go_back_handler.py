dbe904165f399f1de60afd9383fd39fe8553372a
Bulat Gabdrakhmanov <gabdrakhmanov.br@gmail.com>
2024-10-21 14:39:31 +0700

    fix go_back

--- src/handlers/go_back_handler.py
+++ src/handlers/go_back_handler.py
@@ -1,7 +1,8 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import ContextTypes
 
-from src import handlers
+from src.handlers import start_command
+from src.handlers.docker_handlers import docker_menu
 
 async def go_back(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     """Handle the Back button."""
@@ -11,9 +12,9 @@ async def go_back(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     # If the user clicks "Back" while on the container menu, show the Docker menu
     action = query.data
     if action == "docker_back":
-        return await handlers.start_handler(update, context)
+        return await start_command(update, context)
     elif action == "container_back":
         context.user_data.pop("selected_container", None)
-        return await handlers.docker_menu(update, context)
+        return await docker_menu(update, context)
     else:
-        return await handlers.start_handler(update, context)
+        return await start_command(update, context)
